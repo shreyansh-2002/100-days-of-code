172702d353088e411363345311ef712d46adb988
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-01-19 17:16:39 +0530

    Day 19 progress

---
+++ Day19/DSA/Subarray_Sum_Equals_K.java
@@ -0,0 +1,32 @@
+Question:-  https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+Code:- 
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+
+       int sum = 0;
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) 
+        {
+            sum = 0;
+
+            for (int j = i; j < nums.length; j++) 
+            {
+                sum += nums[j];
+
+                if (sum == k) 
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+        
+        
+    }
+}
---
+++ Day19/DSA/Subarray_Sum_Equals_k.cpp
@@ -0,0 +1,30 @@
+Question:- https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+Code:- 
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+
+        int sum = 0;
+
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            sum = 0;
+
+            for (int j = i; j < nums.size(); j++) 
+            {
+                sum += nums[j];
+
+                if (sum == k) 
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
