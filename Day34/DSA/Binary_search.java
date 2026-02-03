6f47a7cec84c6d23037ecf7a2fb57d366ec75461
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-02-03 15:23:56 +0530

    Day 34 progress

---
+++ Day34/DSA/Binary_search.java
@@ -0,0 +1,30 @@
+Question:-  https://leetcode.com/problems/binary-search/description/
+
+Code:-  
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+
+        int high = nums.length - 1;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(target > nums[mid])
+            {
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
