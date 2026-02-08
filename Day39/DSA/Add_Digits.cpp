a8e757684602b799fc470af9b325c48bd83b47f7
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-02-08 20:36:26 +0530

    Day 39 progress

---
+++ Day39/DSA/Add_Digits.cpp
@@ -0,0 +1,19 @@
+Code:-
+
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        while(sum>9 || num>0)
+        {
+            if(num == 0)
+            {
+                num = sum;
+                sum = 0;
+            }
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
+};
