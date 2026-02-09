c6d45ea97daf82a9b6add8305234befa33f458a4
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-02-09 16:59:57 +0530

    Day 40 Progress

---
+++ Day40/DSA/Find_the_Distance_Value_Between_Two_Arrays.java
@@ -0,0 +1,30 @@
+Question:- https://leetcode.com/problems/find-the-distance-value-between-two-arrays/description/?envType=problem-list-v2&envId=sorting
+
+Code:-
+
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+
+        int count = 0;
+        
+        for(int i = 0;i<arr1.length;i++)
+        {
+            boolean flag = true;
+            for(int j = 0;j<arr2.length;j++)
+            {
+                if(Math.abs(arr1[i] - arr2[j]) <= d)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
