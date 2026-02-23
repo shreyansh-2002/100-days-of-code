d06e482eccc3e7650fa7bbfbab6f3533862a1800
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-02-23 16:48:58 +0530

    Day 52 progress

---
+++ Day52/DSA/Count_Number_of_Pairs_With_Absolute_Difference_K.java
@@ -0,0 +1,20 @@
+Question:-  https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/description/
+
+Code:-
+
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+
+        int cnt = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
---
+++ Day52/DSA/Count_Number_of_pairs_with_absolute_difference_k.cpp
@@ -0,0 +1,21 @@
+Question:-  https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/description/
+
+
+Code:-
+
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int cnt = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i; j < nums.size(); j++) {
+                if (abs(nums[i] - nums[j]) == k) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
