19dd675b65dd9e3291827e487bb8aecbabe8774e
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-03-01 17:12:28 +0530

    Day 58 progress

---
+++ Day58/DSA/Find_the_pivot_Integer.cpp
@@ -0,0 +1,27 @@
+Question:-  https://leetcode.com/problems/find-the-pivot-integer/description/?envType=problem-list-v2&envId=maths-m1-arithmetic-basic-reasoning
+
+Code:-
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+
+        int s1 = 0;
+        int s2 = 0;
+
+        for(int i = n;i>=1;i--)
+        {
+            s1 = i*(i+1)/2;
+            
+            s2 += i;
+
+            if(s1 == s2)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+        
+    }
+};
