4ca2605537e08565fcc35f83e6c5571f157f9270
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-03-06 16:12:31 +0530

    Day 62 progress

---
+++ Day62/DSA/Append_Characters_to_String_to_Make_Subsequence.cpp
@@ -0,0 +1,23 @@
+Question:- https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/
+
+Code:- 
+
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i = 0;
+        int j = 0;
+
+        while(i < s.size() && j < t.size())
+        {
+            if(s[i] == t[j])
+            {
+                j++;
+            }
+
+            i++;
+        }
+
+        return t.size() - j;
+    }
+};
