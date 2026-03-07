d1896bf43c3ffdee734e1217b66383e6ffa26970
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-03-07 16:38:14 +0530

    Day 63 progress

---
+++ Day63/DSA/Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,19 @@
+Question:-  https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/?envType=problem-list-v2&envId=array
+
+Code:- 
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        
+        for(int i = 0;i<letters.size();i++)
+        {
+            if(letters[i] > target)
+            {
+                return letters[i];
+            }
+        }
+
+        return letters[0];
+    }
+};
