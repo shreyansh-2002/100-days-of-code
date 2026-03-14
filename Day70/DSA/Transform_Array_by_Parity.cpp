b6faa67df069b919fa49e4cafe56bd8f2e00af0d
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-03-14 22:50:04 +0530

    Day 70 progress

---
+++ Day70/DSA/Transform_Array_by_Parity.cpp
@@ -0,0 +1,24 @@
+Question:- https://leetcode.com/problems/transform-array-by-parity/
+
+Code:-
+
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(nums[i] % 2 == 0)
+            {
+                nums[i] = 0;
+            }
+            else{
+                nums[i] = 1;
+            }
+        }
+
+        sort(nums.begin(),nums.end());
+
+        return nums;
+    }
+};
---
+++ Day70/DSA/Transform_Array_by_Parity.java
@@ -0,0 +1,23 @@
+Question:-  https://leetcode.com/problems/transform-array-by-parity/description/
+
+Code:-
+
+class Solution {
+    public int[] transformArray(int[] nums) {
+
+        for(int i = 0;i<nums.length;i++)
+        {
+            if(nums[i] % 2 == 0)
+            {
+                nums[i] = 0;
+            }
+            else{
+                nums[i] = 1;
+            }
+        }
+
+        Arrays.sort(nums);
+
+        return nums;
+    }
+}
