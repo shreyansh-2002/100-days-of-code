1343ca31357c4ee4c1a7e8df58a1f89d65a7c916
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-03-24 23:56:19 +0530

    Day 80 progress

---
+++ Day80/DSA/Compute_Alternating_Sum.java
@@ -0,0 +1,19 @@
+Question:- https://leetcode.com/problems/compute-alternating-sum/description/
+
+Code:- 
+
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                sum += nums[i];
+            } else {
+                sum -= nums[i];
+            }
+        }
+
+        return sum;
+    }
+}
