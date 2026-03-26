74adc3507ba5c321bea7cc73ebd0191b1ca12f3b
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-03-26 23:18:03 +0530

    Day 82 progress

---
+++ Day82/DSA/Find_Most_Frequent_Vowel_and_Consonant.cpp
@@ -0,0 +1,21 @@
+Question:- https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/description/
+
+Code:-
+
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        int freq[26], maxVowel = 0, maxConso = 0;
+        for (char c : s) {
+            int i = c - 'a';
+            freq[i]++;
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                maxVowel = max(maxVowel, freq[i]);
+            }
+            else {
+                maxConso = max(maxConso, freq[i]);
+            }
+        }
+        return maxVowel + maxConso;
+    }
+};
