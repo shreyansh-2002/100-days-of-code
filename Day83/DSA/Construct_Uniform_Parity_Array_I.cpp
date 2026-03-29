1695c44bd35b504bda9450518fb167bb51a65f11
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-03-30 00:02:23 +0530

    Day 83 progress

---
+++ Day83/DSA/Construct_Uniform_Parity_Array_I.cpp
@@ -0,0 +1,10 @@
+Question:- https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+
+Code:- 
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
---
+++ Day83/DSA/Construct_Uniform_Parity_Array_I.java
@@ -0,0 +1,9 @@
+Question:- https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+
+Code:- 
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
