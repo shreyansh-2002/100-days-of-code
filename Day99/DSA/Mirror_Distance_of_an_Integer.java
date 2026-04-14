976d883b46976f048bdd196cfb3ca5967f70d3a6
Shreyanshsomani <106816110+shreyansh-2002@users.noreply.github.com>
2026-04-14 23:55:25 +0530

    Day 99 progress

---
+++ Day99/DSA/Mirror_Distance_of_an_Integer.java
@@ -0,0 +1,18 @@
+Question:- https://leetcode.com/problems/mirror-distance-of-an-integer/
+
+Code:- 
+
+class Solution {
+    public int mirrorDistance(int n) {
+        int temp = n;
+        int rev = 0;
+
+        while(temp > 0)
+        {
+            rev = rev*10 + temp%10;
+            temp /= 10;
+        }
+
+        return Math.abs(n-rev);
+    }
+}
